63188e9d754ef980958eaafabef393654b7d446c
Artem <asyuzyaev@lab4u.ru>
2023-10-30 14:06:24 +0300

    added styles

---
+++ src/app/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import "./styles/index.scss"
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <!-- <HelloWorld msg="You did it!" /> -->
+
+      <nav>
+        <div>123123123 </div>
+      </nav>
+    </div>
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped lang="scss">
+header {
+  .wrapper {
+    background-color: yellowgreen;
+  }
+}
+  
+</style>
